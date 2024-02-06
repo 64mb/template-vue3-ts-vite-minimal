b0b41d07375f84a79ff2af6d746c534f60ccabee
Alex Ebenrode <alexebenrode@gmail.com>
2024-02-06 13:42:16 +0500

    added eslint parser and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,4 +19,49 @@ module.exports = {
     project: ["./tsconfig.json"],
   },
   plugins: ["vue", "vitest"],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    project: ["tsconfig.json"],
+    parser: {
+      ts: '@typescript-eslint/parser',
+    },
+  },
+  rules: {
+    "no-extra-boolean-cast": "off",
+    "vue/no-v-html": "off",
+    "unicorn/prevent-abbreviations": [
+      "error",
+      {
+        replacements: {
+          props: false,
+          prop: false,
+          attrs: false,
+          params: false,
+          req: false,
+          res: false,
+          dev: false,
+          src: false,
+        },
+      },
+    ],
+    "unicorn/filename-case": [
+      "error",
+      {
+        cases: {
+          camelCase: true,
+          pascalCase: true,
+        },
+      },
+    ],
+    "no-restricted-imports": [
+      "error",
+      {
+        patterns: ["../*", "lodash.*"],
+      },
+    ],
+    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "require-await": "error",
+  },
 }
